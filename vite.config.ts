bf9e8330731af40d055aeeebedf0fcd38d270af0
Da1aoshu <Dalaoshu@qq.com>
2023-10-31 12:27:13 +0800

    manual deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/P2P-Multiplayer-Chat-Room/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     vue(),
     AutoImport({
